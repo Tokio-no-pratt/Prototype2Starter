3089e20d0107f28b070ff9d094977ec0443dcc7f
Tokio <suelennascimento164@gmail.com>
2026-03-19 14:00:05 +0000

    final
    final

--- Assets/script/DestroyOutOfBounds.cs
+++ Assets/script/DestroyOutOfBounds.cs
@@ -8,21 +8,22 @@ public class DestroyOutOfBounds : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (transform.position. z > topBound){
+        if (transform.position.z > topBound)
+        {
             Destroy(gameObject);
         }
 
-        else if (transform.position. z < lowerBound)
+        else if (transform.position.z < lowerBound)
         {
             Debug.Log("Game Over!");
             Destroy(gameObject);
         }
-        
+
     }
 }
